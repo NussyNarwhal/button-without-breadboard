3ecc1d93d2e2e35be6b9238860fc4e3db61b994e
NussyNarwhal <98988961+NussyNarwhal@users.noreply.github.com>
2022-08-31 16:18:47 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showIcon(IconNames.Asleep)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+})
+basic.showIcon(IconNames.Yes)
